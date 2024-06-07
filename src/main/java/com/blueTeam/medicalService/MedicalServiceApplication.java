a508681f64e43883c790d46c55090021bc30ad22
Юра Кондратенко <antooon050902@yandex.com>
2024-06-07 21:12:13 +0300

    initial commit

---
+++ src/main/java/com/blueTeam/medicalService/MedicalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blueTeam.medicalService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blueTeam/medicalService/MedicalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueTeam.medicalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
