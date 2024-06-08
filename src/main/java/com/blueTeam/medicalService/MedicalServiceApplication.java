56738372ec33efd00e515fef6255338270ff1a02
yurchikKon <157982309+yurchikKon@users.noreply.github.com>
2024-06-08 12:24:55 +0300

    Update MedicalServiceApplication.java

--- src/main/java/com/blueTeam/medicalService/MedicalServiceApplication.java
+++ src/main/java/com/blueTeam/medicalService/MedicalServiceApplication.java
@@ -2,8 +2,6 @@ package com.blueTeam.medicalService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MedicalServiceApplication {
