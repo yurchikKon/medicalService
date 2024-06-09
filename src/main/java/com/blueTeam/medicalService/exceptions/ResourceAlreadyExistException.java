ed00faa774ea26563c078e93472897e2aba3dfd4
Юра Кондратенко <antooon050902@yandex.com>
2024-06-09 12:50:01 +0300

    exception directory added

---
+++ src/main/java/com/blueTeam/medicalService/exceptions/ResourceAlreadyExistException.java
@@ -0,0 +1,10 @@
+package com.blueTeam.medicalService.exceptions;
+
+/**
+ * todo Document type ResourceAlreadyExistException
+ */
+public class ResourceAlreadyExistException extends RuntimeException{
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
+}
