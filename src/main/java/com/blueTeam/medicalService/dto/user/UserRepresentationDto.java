7141808b2e8b2feb1187513f4b7ab86458ef647a
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2024-06-09 23:01:50 +0300

    fix: fix inheritance in @Builder

--- src/main/java/com/blueTeam/medicalService/dto/user/UserRepresentationDto.java
+++ src/main/java/com/blueTeam/medicalService/dto/user/UserRepresentationDto.java
@@ -1,14 +1,18 @@
 package com.blueTeam.medicalService.dto.user;
 
 import com.blueTeam.medicalService.entities.enums.Role;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
 
 import static lombok.AccessLevel.PRIVATE;
 
 @Data
-@Builder
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = PRIVATE)
 public class UserRepresentationDto {
     Long id;
