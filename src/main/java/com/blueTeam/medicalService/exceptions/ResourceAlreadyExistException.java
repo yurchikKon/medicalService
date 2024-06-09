6431dfd7692b6e83451040b10d33f55e251396c6
Юра Кондратенко <antooon050902@yandex.com>
2024-06-09 12:52:50 +0300

    ResourceAlreadyExistException added

--- src/main/java/com/blueTeam/medicalService/exceptions/ResourceAlreadyExistException.java
+++ src/main/java/com/blueTeam/medicalService/exceptions/ResourceAlreadyExistException.java
@@ -1,8 +1,5 @@
 package com.blueTeam.medicalService.exceptions;
 
-/**
- * todo Document type ResourceAlreadyExistException
- */
 public class ResourceAlreadyExistException extends RuntimeException{
     public ResourceAlreadyExistException(String message) {
         super(message);
