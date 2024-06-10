74fd41b70d52d9e708bb14e3a543f6684af69901
Andru <pudowkin98@mail.ru>
2024-06-10 15:39:08 +0300

    MED-20 - finished

--- src/main/java/com/blueTeam/medicalService/services/implementations/AnalysisDirectionServiceImpl.java
+++ src/main/java/com/blueTeam/medicalService/services/implementations/AnalysisDirectionServiceImpl.java
@@ -1,8 +1,22 @@
 package com.blueTeam.medicalService.services.implementations;
 
+import com.blueTeam.medicalService.entities.AnalysisDirection;
+import com.blueTeam.medicalService.entities.enums.Usage;
+import com.blueTeam.medicalService.repositories.AnalysisDirectionRepository;
 import com.blueTeam.medicalService.services.interfaces.AnalysisDirectionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnalysisDirectionServiceImpl implements AnalysisDirectionService {
+    @Autowired
+    private AnalysisDirectionRepository analysisDirectionRepository;
+    @Override
+    public void changeResultsAnalysisDirection(Long idAnalysisDirection, String newResult) {
+        AnalysisDirection analysisDirection = analysisDirectionRepository.findById(idAnalysisDirection).get();
+        if(analysisDirection.getUsage()==Usage.USED){
+            analysisDirection.setResult(newResult);
+        }
+        analysisDirectionRepository.save(analysisDirection);
+    }
 }
--- src/main/java/com/blueTeam/medicalService/services/interfaces/AnalysisDirectionService.java
+++ src/main/java/com/blueTeam/medicalService/services/interfaces/AnalysisDirectionService.java
@@ -1,5 +1,10 @@
 package com.blueTeam.medicalService.services.interfaces;
 
 
+import com.blueTeam.medicalService.entities.AnalysisDirection;
+
+import java.util.List;
+
 public interface AnalysisDirectionService {
+    public void changeResultsAnalysisDirection(Long idAnalysisDirection, String newResult);
 }
