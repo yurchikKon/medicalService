5a932d3cc09fc2338d88107a9d12c8047edab5aa
Andru <pudowkin98@mail.ru>
2024-06-11 23:57:37 +0300

    Устранение замечаний - ок

--- src/main/java/com/blueTeam/medicalService/services/implementations/AnalysisDirectionServiceImpl.java
+++ src/main/java/com/blueTeam/medicalService/services/implementations/AnalysisDirectionServiceImpl.java
@@ -4,27 +4,25 @@ import com.blueTeam.medicalService.entities.AnalysisDirection;
 import com.blueTeam.medicalService.entities.enums.Usage;
 import com.blueTeam.medicalService.repositories.AnalysisDirectionRepository;
 import com.blueTeam.medicalService.services.interfaces.AnalysisDirectionService;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class AnalysisDirectionServiceImpl implements AnalysisDirectionService {
-    @Autowired
-    private AnalysisDirectionRepository analysisDirectionRepository;
+
+    private final AnalysisDirectionRepository analysisDirectionRepository;
     @Override
     public void changeResultsAnalysisDirection(Long idAnalysisDirection, String newResult) {
-        AnalysisDirection analysisDirection = null;
-       Optional<AnalysisDirection> analysisDirectionOptional = analysisDirectionRepository.findById(idAnalysisDirection);
-        if (analysisDirectionOptional.isPresent()) {
-            analysisDirection = analysisDirectionOptional.get();
+        AnalysisDirection analysisDirection = analysisDirectionRepository.findById(idAnalysisDirection)
+                .orElseThrow(() -> new EntityNotFoundException("Invalid id: " + idAnalysisDirection));
             if(analysisDirection.getUsage()==Usage.USED){
                 analysisDirection.setResult(newResult);
-            }
-            analysisDirectionRepository.save(analysisDirection);
+                analysisDirectionRepository.save(analysisDirection);
+                log.info("Изменена запись в результатах анализа на: {}", newResult);
+            }else log.info("Выбранный анализ имеет статус UNUSED.");
         }
-    }
 }
