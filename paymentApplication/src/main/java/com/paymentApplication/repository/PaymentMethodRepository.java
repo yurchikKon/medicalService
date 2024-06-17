90c9cf62e3c1fd09e87b928c9d461fcf23afe2e7
Ermakov Alexandr <ermakov.sascha2015yandex.ru@gmail.com>
2024-06-17 22:05:18 +0300

    feat: create repositories to DB model

---
+++ paymentApplication/src/main/java/com/paymentApplication/repository/PaymentMethodRepository.java
@@ -0,0 +1,9 @@
+package com.paymentApplication.repository;
+
+import com.paymentApplication.model.entity.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+}
---
+++ paymentApplication/src/main/java/com/paymentApplication/repository/StatusRepository.java
@@ -0,0 +1,9 @@
+package com.paymentApplication.repository;
+
+import com.paymentApplication.model.entity.Status;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StatusRepository extends JpaRepository<Status, Long> {
+}
