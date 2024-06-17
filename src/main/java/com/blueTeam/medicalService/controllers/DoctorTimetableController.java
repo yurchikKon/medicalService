5855172d99feba523f31c56ba91b8c565ab9c84e
AlexandrDaineka <zanoza193@mail.ru>
2024-06-17 09:19:27 +0300

    Two tasks appointments_doctors and doctors_timetable

--- src/main/java/com/blueTeam/medicalService/controllers/DoctorTimetableController.java
+++ src/main/java/com/blueTeam/medicalService/controllers/DoctorTimetableController.java
@@ -1,11 +1,38 @@
 package com.blueTeam.medicalService.controllers;
 
+import com.blueTeam.medicalService.dto.user.doctor.timetable.DoctorTimetableDto;
+import com.blueTeam.medicalService.entities.DoctorTimetable;
+import com.blueTeam.medicalService.services.interfaces.DoctorTimetableService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/doctorTimetables")
+@Slf4j
 public class DoctorTimetableController {
+    private final DoctorTimetableService doctorTimetableService;
+    @GetMapping
+    public ResponseEntity<List<DoctorTimetableDto>> getDoctorTimetablesByDayOfWeek(
+            @RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        if (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) {
+            String errorMessage = "Выбран недопустимый день недели: " + dayOfWeek.toString();
+            log.error(errorMessage);
+            return ResponseEntity.badRequest().body(null);
+        }
+        List<DoctorTimetableDto> timetables = doctorTimetableService.findDoctorTimetablesByDayOfWeek(dayOfWeek);
+        return ResponseEntity.ok(timetables);
+    }
 }
--- src/main/java/com/blueTeam/medicalService/repositories/DoctorAppointmentRepository.java
+++ src/main/java/com/blueTeam/medicalService/repositories/DoctorAppointmentRepository.java
@@ -2,8 +2,17 @@ package com.blueTeam.medicalService.repositories;
 
 import com.blueTeam.medicalService.entities.DoctorAppointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface DoctorAppointmentRepository extends JpaRepository<DoctorAppointment, Long> {
+    @Query("SELECT d FROM DoctorAppointment d WHERE cast(d.dateTime as date)= :date")
+    List<DoctorAppointment> findAllByDateTime(@Param("date") LocalDate localDate);
+
 }
