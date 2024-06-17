796cbae18ace301034b4de5a581b6b500974b2c6
Nika Nesterenko <veronicanesterenko@mail.ru>
2024-06-17 14:44:03 +0300

    add logic for patient viewing active analysis directions

--- src/main/java/com/blueTeam/medicalService/services/interfaces/PatientService.java
+++ src/main/java/com/blueTeam/medicalService/services/interfaces/PatientService.java
@@ -1,5 +1,11 @@
 package com.blueTeam.medicalService.services.interfaces;
 
 
+import com.blueTeam.medicalService.dto.analysis.AnalysisDirectionDto;
+
+import java.util.List;
+
 public interface PatientService {
+    List<AnalysisDirectionDto> findActivePatientAnalysisDirections(Long patientId);
+
 }
