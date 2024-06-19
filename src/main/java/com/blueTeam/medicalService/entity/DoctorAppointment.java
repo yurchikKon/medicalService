e482069d1d9a5f56f97f2a185cf031e0f05dd5f1
Nika Nesterenko <veronicanesterenko@mail.ru>
2024-06-19 12:06:32 +0300

    Merge branch 'master' into feature/view_patient_appointments
    # Conflicts:
    #	src/main/java/com/blueTeam/medicalService/controller/PatientController.java
    remove unused imports

--- src/main/java/com/blueTeam/medicalService/entity/DoctorAppointment.java
+++ src/main/java/com/blueTeam/medicalService/entity/DoctorAppointment.java
@@ -7,8 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.JdbcType;
-import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 
 import java.time.LocalDateTime;
 import java.util.List;
