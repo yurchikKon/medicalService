86b3cc5f3fbe61cbb2442f8df123f059b32c1210
Юра Кондратенко <antooon050902@yandex.com>
2024-06-24 10:16:23 +0300

    resolve merge problems

--- src/main/java/com/blueTeam/medicalService/service/implementation/DoctorAppointmentServiceImpl.java
+++ src/main/java/com/blueTeam/medicalService/service/implementation/DoctorAppointmentServiceImpl.java
@@ -40,6 +40,7 @@ public class DoctorAppointmentServiceImpl implements DoctorAppointmentService {
     private final DoctorAppointmentMapper doctorAppointmentMapper;
     private final DoctorMapper doctorMapper;
 
+
     @Transactional(readOnly = true)
     @Override
     public List<DoctorAppointmentRepresentationDto> findAllScheduledByDoctorIdAndDate(Long id, LocalDate localDate) {
