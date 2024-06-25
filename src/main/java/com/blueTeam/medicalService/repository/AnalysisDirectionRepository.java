3fbf706cbd00d85c01f074e12ec0d5cf8a0f4cb8
AndruPudovkin <135853431+AndruPudovkin@users.noreply.github.com>
2024-06-26 01:28:28 +0300

    merge commit

--- src/main/java/com/blueTeam/medicalService/repository/AnalysisDirectionRepository.java
+++ src/main/java/com/blueTeam/medicalService/repository/AnalysisDirectionRepository.java
@@ -1,12 +1,7 @@
 package com.blueTeam.medicalService.repository;
 
-<<<<<<< HEAD:src/main/java/com/blueTeam/medicalService/repositories/AnalysisDirectionRepository.java
-import com.blueTeam.medicalService.dto.analysis.AnalysisDirectionDto;
-import com.blueTeam.medicalService.entities.AnalysisDirection;
-import com.blueTeam.medicalService.entities.enums.Usage;
-=======
+
 import com.blueTeam.medicalService.entity.AnalysisDirection;
->>>>>>> master:src/main/java/com/blueTeam/medicalService/repository/AnalysisDirectionRepository.java
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
