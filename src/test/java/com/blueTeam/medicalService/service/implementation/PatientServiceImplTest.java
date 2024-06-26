cb03a9f3e8d53c8e8b7b145c9ec8efc337c91223
K4lte <verlornes@gmail.com>
2024-06-26 22:29:27 +0300

    MED-4: Added tests

--- src/test/java/com/blueTeam/medicalService/service/implementation/PatientServiceImplTest.java
+++ src/test/java/com/blueTeam/medicalService/service/implementation/PatientServiceImplTest.java
@@ -18,7 +18,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -35,27 +34,6 @@ class PatientServiceImplTest {
 
     @InjectMocks
     private PatientServiceImpl patientService;
-
-    @Test
-    void findActivePatientAnalysisDirections_ShouldReturnResult() {
-        Long patientId = 1L;
-        var patient = createPatient(patientId);
-        DoctorAppointment appointment = createAppointment(patient);
-        AnalysisDirection direction = createAnalysisDirection(appointment);
-        List<AnalysisDirectionDto> expectedDtos = Stream.of(direction)
-                .map(t -> createDto(t.getId()))
-                .toList();
-
-        when(doctorAppointmentRepository.findAllByPatientIdAndStatus(patientId, Status.COMPLETED))
-                .thenReturn(List.of(appointment));
-        List.of(direction).forEach(t -> when(mapper.mapToDto(t)).thenReturn(createDto(t.getId())));
-
-        List<AnalysisDirectionDto> actualDtos = patientService.findActivePatientAnalysisDirections(patientId);
-
-        assertEquals(actualDtos, expectedDtos);
-        verify(doctorAppointmentRepository, times(1)).findAllByPatientIdAndStatus(patientId, Status.COMPLETED);
-        List.of(direction).forEach(t -> verify(mapper, times(1)).mapToDto(direction));
-    }
 
     @Test
     void isPatientPresent_ShouldReturnTrue() {
@@ -79,29 +57,4 @@ class PatientServiceImplTest {
         verify(patientRepository, times(1)).findById(patientId);
     }
 
-    private DoctorAppointment createAppointment(Patient patient) {
-        DoctorAppointment appointment = new DoctorAppointment();
-        appointment.setPatient(patient);
-        appointment.setStatus(Status.COMPLETED);
-        return appointment;
-    }
-
-    private Patient createPatient(Long id) {
-        Patient patient = new Patient();
-        patient.setId(id);
-        return patient;
-    }
-
-    private AnalysisDirection createAnalysisDirection(DoctorAppointment appointment) {
-        AnalysisDirection direction = new AnalysisDirection();
-        direction.setId(1L);
-        direction.setStatus(DirectionStatus.VALID);
-        direction.setDoctorsAppointment(appointment);
-        appointment.setAnalysisDirections(List.of(direction));
-        return direction;
-    }
-
-    private AnalysisDirectionDto createDto(Long id) {
-        return new AnalysisDirectionDto(id, DirectionStatus.VALID, Usage.UNUSED, null);
-    }
 }
